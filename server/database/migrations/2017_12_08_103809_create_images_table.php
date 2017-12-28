9a2c083c6c6695fd2499a2983db6f172d7d378ef
sebastabera <sebas03700@hotmail.com>
2017-12-28 04:58:57 -0500

    cambios

---
+++ server/database/migrations/2017_12_08_103809_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255)->nullable();
+            $table->text('description')->nullable();
+            $table->string('thumbnail', 1000)->nullable();
+            $table->string('imageLink', 1000)->nullable();
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
